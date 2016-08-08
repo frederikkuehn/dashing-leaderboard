1aae1d77364efd04b13147eda31b7902baaf6e25
Frederik Kuehn <mail@frederikkuehn.com>
2016-08-07 19:03:33 -0700

    First version

---
+++ jobs/slack_gamebot_leaderboard.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+require 'json'
+
+gamebot_url = 'GAMEBOT_URL'
+team_id = 'TEAM_ID'
+
+SCHEDULER.every '30s', :first_in => 0 do |job|
+  uri = URI("#{gamebot_url}api/seasons/current/?team_id=#{team_id}")
+  response = Net::HTTP.get(uri)
+  data = JSON.parse(response)
+  result = Array.new
+  data['_embedded']['user_ranks'].each do |standing|
+    result.push({
+      rank: "#{standing['rank']}.",
+      player: "#{standing['user_name']}:",
+      wins: "#{standing['wins']} win#{standing['wins'] != 1 ? 's' : ''},",
+      losses: "#{standing['losses']} loss#{standing['losses'] != 1 ? 'es' : ''}",
+      elo: "(elo: #{standing['elo']})"
+    })
+  end
+  send_event('gamebot-leaderboard', { standings: result })
+end
